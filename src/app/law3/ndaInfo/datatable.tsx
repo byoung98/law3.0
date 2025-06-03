36edd11f3ac7fa53ad0a2d2fa6ce61e9630cdfcc
Brittany Young <youngbrittany2198@gmail.com>
2025-06-02 23:00:15 -0500

    adding datatable data

---
+++ src/app/law3/ndaInfo/datatable.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+//import {ndaInfo} from '@/app/ndaInfo/placeholder-date';
+export default function Page(){
+ return (
+    <>
+    
+    </>
+ )   
+}
---
+++ src/app/law3/ndaInfo/placeholder-data.js
@@ -0,0 +1,37 @@
+const table = [
+{
+    ndaID:"ICMNDADMA123",
+    ndaType: "Master",
+    agreementType:"standard",
+    status:"Executed",
+    otherParty:"Test",
+    endDate:"Evergreen",
+    requesterName:"Chris Fowler"
+},
+{
+    ndaID:"ICMNDADMA183",
+    ndaType: "Master",
+    agreementType:"standard",
+    status:"Executed",
+    otherParty:"Other Country - Test",
+    endDate:"Evergreen",
+    requesterName:"Jennifer Anniston"
+},
+{
+    ndaID:"ICMNDADMA199",
+    ndaType: "Master",
+    agreementType:"standard",
+    status:"Pending",
+    otherParty:"N/A",
+    endDate:"Evergreen",
+    requesterName:"Jane Doe"
+}
+
+
+
+
+
+
+
+
+]
