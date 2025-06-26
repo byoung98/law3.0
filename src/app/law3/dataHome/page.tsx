093ac96bcb1028eb048545dd4610ba0125cb0990
Brittany Young <youngbrittany2198@gmail.com>
2025-06-25 21:30:08 -0500

    resolved log error

--- src/app/law3/dataHome/page.tsx
+++ src/app/law3/dataHome/page.tsx
@@ -11,8 +11,8 @@ import {DataTable} from "@/app/law3/ndaInfo/datatable";
 // });
 
 async function fetchData(): Promise<NDAs[]> {
-    const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
-    const response = await fetch(`${baseUrl}/api/ndas`, { cache: 'no-store' });
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+  const response = await fetch(`${baseUrl}/api/ndas`, { cache: 'no-store' });
   
     if (!response.ok) {
       throw new Error("Failed to fetch data from the database");
